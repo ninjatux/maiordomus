816c7cda7a8350e64c1ac6b3526677d6b3b077ae
Valerio Barrila <valerio.barrila@gmail.com>
2014-07-30 01:13:54 +0200

    refactor

---
+++ modules/base.js
@@ -0,0 +1,11 @@
+var logger = require('../lib/logger'),
+    Promise = require('bluebird');
+
+module.exports = {
+    log: function (message, maiordomus) {
+        return new Promise(function (resolve, reject) {
+            logger.user(maiordomus.$currentStep.name, message[0].toString());
+            resolve();
+        });
+    }
+};
